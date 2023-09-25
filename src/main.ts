7de6b5f7255af8dff33a91602637c1d18c50735d
rushil <rushil.aipl@gmail.com>
2023-09-25 15:11:14 +0530

    addition pf postgres

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views')); 
   app.setViewEngine('ejs');
-  await app.listen(3000);
+  await app.listen((process.env.PORT||3000),()=>{
+    console.log("server started on port " + process.env.PORT);
+    
+  });
 }
 bootstrap();
